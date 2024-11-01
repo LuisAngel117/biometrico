9ecd4943cceb251a00090e1eaf15cf7995c486ed
LuisAngel117 <luis.angel1995117@gmail.com>
2024-11-01 10:24:16 -0500

    primer commit

---
+++ src/test/java/com/sistema/biometrico/BiometricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.biometrico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cb6f053189fb99ed9950c60f4cac2da54624a77c
julio.proyectos <julio.viznay.proyectos@gmail.com>
2024-11-02 00:53:36 -0500

    validaciones empleado

--- src/test/java/com/sistema/biometrico/BiometricoApplicationTests.java
+++ src/test/java/com/sistema/biometrico/BiometricoApplicationTests.java
@@ -1,13 +1,48 @@
 package com.sistema.biometrico;
 
+import static org.assertj.core.api.Assertions.as;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.x509;
+
+import java.security.PrivateKey;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sistema.biometrico.entidad.Empleado;
+import com.sistema.biometrico.repositorio.IEmpleadoRepositorio;
+import com.sistema.biometrico.servicio.IEmpleadoServicio;
+import com.sistema.biometrico.servicioImpl.EmpleadoServicioImpl;
+
+
 
 @SpringBootTest
 class BiometricoApplicationTests {
 
+	//@Autowired
+	//private EmpleadoServicioImpl servicio;
+	
 	@Test
 	void contextLoads() {
+	
+		//List<Empleado> emp=servicio.listarEmpleadosActivos();
+		
+		//for(Empleado empleado : emp){
+			//System.out.println(empleado.getId());
+		//}
+		
+		
+		//emp.setCedula("1234567890");
+		//emp.setNombre("Vizzñay");
+		//emp.setApellido("Julio");
+		//emp.setDireccion("Cutuglagua");
+		//emp.setTelefono("123344");
+		//emp.setCorreo("julio@gmaila.com");
+		//emp.setEstado(false);
+		//servicio.eliminar(1, emp);
+		
+		
 	}
 
 }
